2e2533b4e282682a5ac04f15d1a4fc5bbd3d2fa4
Eun-Hee <hd96010@daum.net>
2021-03-14 14:22:24 +0900

    commemt

--- app/src/main/java/com/tjoeun/pizzaorderapp_20210314/MainActivity.kt
+++ app/src/main/java/com/tjoeun/pizzaorderapp_20210314/MainActivity.kt
@@ -7,6 +7,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
+//    뷰페이저에 프래그먼트를 뿌려주기 위한 어댑터 변수
+//    lateinit var 로
+
     lateinit var mvpa : MyViewpagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,9 +24,14 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        ViewPager 에 연결될 어댑터를 실제 객체화 (메모리 등록)
         mvpa = MyViewpagerAdapter(supportFragmentManager)
 
+//        메인화면에 있는 myViewPager의 adpater로, mvpa를 등록
         myViewPager.adapter = mvpa
+
+//        탭레이아웃을 => myViewpage
         myTabLayout.setupWithViewPager(myViewPager)
 
 
