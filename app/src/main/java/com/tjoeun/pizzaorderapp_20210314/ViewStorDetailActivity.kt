00e4f77b409006ca222624634eedc381040cd110
Eun-Hee <hd96010@daum.net>
2021-03-14 13:46:10 +0900

    전화 권한 체크 / 실제 전화 연결

--- app/src/main/java/com/tjoeun/pizzaorderapp_20210314/ViewStorDetailActivity.kt
+++ app/src/main/java/com/tjoeun/pizzaorderapp_20210314/ViewStorDetailActivity.kt
@@ -1,11 +1,17 @@
 package com.tjoeun.pizzaorderapp_20210314
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.service.autofill.UserData
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.TedPermission
 import com.tjoeun.pizzaorderapp_20210314.datas.Store
 import kotlinx.android.synthetic.main.activity_view_stor_detail.*
+import java.security.Permission
+import java.util.jar.Manifest
 
 class ViewStorDetailActivity : BaseActivity() {
 
@@ -16,8 +22,27 @@ class ViewStorDetailActivity : BaseActivity() {
         setContentView(R.layout.activity_view_stor_detail)
         setupEvents()
         setValues()
+    }
 
     override fun setupEvents() {
+
+        orderCallBtn.setOnClickListener {
+            val pl = object : PermissionGranted() {
+
+                val myUri = Uri.parse("tel:${mStoreData.phoneNum}")
+                val myItent = Intent(Intent.ACTION_CALL,myUri)
+                startActivity(myIntent)
+
+            }
+            TedPermission.with(mContext)
+                .setPermissions(pl)
+                .setDeniedMessage("[설정] 에서 통화 권한을 허락해주세요")
+                .setPermissions(Manifest.permission.call_)
+                .check()
+
+           overide fun
+
+        }
 
     }
 
@@ -34,5 +59,4 @@ class ViewStorDetailActivity : BaseActivity() {
     }
 
 
-    }
 }
