a8770ef24ceeae95ca57687c8781693cf725a636
Eun-Hee <hd96010@daum.net>
2021-03-14 12:49:53 +0900

    상세작업

--- app/src/main/java/com/tjoeun/pizzaorderapp_20210314/ViewStorDetailActivity.kt
+++ app/src/main/java/com/tjoeun/pizzaorderapp_20210314/ViewStorDetailActivity.kt
@@ -3,7 +3,9 @@ package com.tjoeun.pizzaorderapp_20210314
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.service.autofill.UserData
+import com.bumptech.glide.Glide
 import com.tjoeun.pizzaorderapp_20210314.datas.Store
+import kotlinx.android.synthetic.main.activity_view_stor_detail.*
 
 class ViewStorDetailActivity : BaseActivity() {
 
@@ -22,6 +24,12 @@ class ViewStorDetailActivity : BaseActivity() {
     override fun setValues() {
 
         mStoreData = intent.getSerializableExtra("store") as Store
+
+        nameTxt.text = mStoreData.StoreName
+        phoneTxt.text = mStoreData.phoneNum
+
+        Glide.with(mContext).load(mStoreData.logourl).into(logoImg)
+
 
     }
 
