6099bb21f37d697bfaad2865868885db9d6b6b63
Eun-Hee <hd96010@daum.net>
2021-03-14 11:10:09 +0900

    내 프로필 프래그먼트 추가

---
+++ app/src/main/java/com/tjoeun/pizzaorderapp_20210314/fragments/MyProfileFregment.kt
@@ -0,0 +1,24 @@
+package com.tjoeun.pizzaorderapp_20210314.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.tjoeun.pizzaorderapp_20210314.R
+
+class MyProfileFregment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fregment_my_profile,container,false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
+}
